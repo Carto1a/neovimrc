9f2eb01526abdd8c68a50f338e2f5787fb2e9918
Cartola <49589600+Carto1a@users.noreply.github.com>
2024-03-27 19:53:07 -0300

    feat: copilot

--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
--- This line makes pacman-installed global Arch Linux vim packages work.
--- source /usr/share/nvim/archlinux.vim
 require("global")
 require("cartola")
 require("teste")
+
--- lua/cartola/lazy/init.lua
+++ lua/cartola/lazy/init.lua
@@ -1,22 +1,23 @@
 return {
-	-- colorshemes
-	{ "sainnhe/gruvbox-material", priority = 1000 },
-	{ "ellisonleao/gruvbox.nvim", priority = 1000 },
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		main = "ibl",
-		opts = {
-			indent = {
-				char = { "▎" },
-				tab_char = "▎",
-			},
-			scope = {
-				enabled = false,
-			},
-		}
-	},
-	"tpope/vim-fugitive",
-	"tpope/vim-commentary",
-	"nvim-treesitter/nvim-treesitter-context",
-	"mbbill/undotree",
+    { "rose-pine/neovim",         priority = 1000, name = "rose-pine" },
+    { "sainnhe/gruvbox-material", priority = 1000 },
+    { "ellisonleao/gruvbox.nvim", priority = 1000 },
+    "github/copilot.vim",
+    {
+        "lukas-reineke/indent-blankline.nvim",
+        main = "ibl",
+        opts = {
+            indent = {
+                char = { "▎" },
+                tab_char = "▎",
+            },
+            scope = {
+                enabled = false,
+            },
+        }
+    },
+    "tpope/vim-fugitive",
+    "tpope/vim-commentary",
+    "nvim-treesitter/nvim-treesitter-context",
+    "mbbill/undotree",
 }
