4a4ad58edfa64036428fea0e743caaae81b68bb8
Carto1a <49589600+Carto1a@users.noreply.github.com>
2024-11-06 18:00:47 -0300

    feat: lazy undotree

--- lua/cartola/lazy/undotree.lua
+++ lua/cartola/lazy/undotree.lua
@@ -1,5 +1,8 @@
 return {
     "mbbill/undotree",
+    lazy = true,
+    cmd = { "UndotreeToggle", "UndotreeShow" },
+    keys = { "<leader>u" },
 
     config = function()
         vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
