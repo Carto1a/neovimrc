af42132c89159bf136f1f83a768a66c6a5afc218
Cartola <49589600+Carto1a@users.noreply.github.com>
2024-11-06 22:06:54 -0300

    wip: update trouble

--- lua/cartola/lazy/trouble.lua
+++ lua/cartola/lazy/trouble.lua
@@ -1,22 +1,37 @@
 return {
-	"folke/trouble.nvim",
-    tag = "v2.10.0",
-	config = function()
-		local setmap = vim.keymap.set
-		local trouble = require("trouble")
-		trouble.setup({
-			icons = false,
-		})
+    "folke/trouble.nvim",
+    -- tag = "v2.10.0",
+    tag = "v3.6.0",
+    config = function()
+        local setmap = vim.keymap.set
+        local trouble = require("trouble")
+        trouble.setup({
+            -- icons = false,
+        })
 
-		setmap("n", "<leader>tt", function() trouble.toggle() end)
-		setmap("n", "<leader>tw", function() trouble.toggle("workspace_diagnostics") end)
-		setmap("n", "<leader>td", function() trouble.toggle("document_diagnostics") end)
-		setmap("n", "<leader>tq", function() trouble.toggle("quickfix") end)
-		setmap("n", "<leader>tl", function() trouble.toggle("loclist") end)
-		setmap("n", "<leader>[t", function() trouble.next({ skip_groups = true, jump = true }) end)
-		setmap("n", "<leader>]t", function() trouble.previous({ skip_groups = true, jump = true }) end)
-		setmap("n", "gr", function() trouble.toggle("lsp_references") end)
-		setmap("n", "gd", function() trouble.toggle("lsp_definitions") end)
-		setmap("n", "gi", function() trouble.toggle("lsp_implementations") end)
-	end,
+        local last_mode = "document_diagnostics"
+
+        local function open_menu(mode)
+            print(mode)
+            print(last_mode)
+            if mode == nil then
+                trouble.toggle(last_mode)
+                return
+            end
+
+            last_mode = mode
+            trouble.toggle(mode)
+        end
+
+        setmap("n", "<leader>tt", function() open_menu() end)
+        setmap("n", "<leader>tw", function() open_menu("workspace_diagnostics") end)
+        setmap("n", "<leader>td", function() open_menu("document_diagnostics") end)
+        setmap("n", "<leader>tq", function() open_menu("quickfix") end)
+        setmap("n", "<leader>tl", function() open_menu("loclist") end)
+        setmap("n", "<leader>[t", function() trouble.next({ skip_groups = true, jump = true }) end)
+        setmap("n", "<leader>]t", function() trouble.previous({ skip_groups = true, jump = true }) end)
+        setmap("n", "gr", function() open_menu("lsp_references") end)
+        setmap("n", "gd", function() open_menu("lsp_definitions") end)
+        setmap("n", "gi", function() open_menu("lsp_implementations") end)
+    end,
 }
