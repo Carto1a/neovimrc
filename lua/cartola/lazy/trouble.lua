c0bd09e4f94cba2ceca56216d42179dbcd536593
Carto1a <49589600+Carto1a@users.noreply.github.com>
2025-06-10 16:59:17 -0300

    refactor(trouble): plugin

--- lua/cartola/lazy/trouble.lua
+++ lua/cartola/lazy/trouble.lua
@@ -1,50 +1,55 @@
+local open_menu = require("cartola.utils.util-trouble").open_menu;
+
 return {
     "folke/trouble.nvim",
-    tag = "v3.6.0",
-    config = function()
-        local setmap = vim.keymap.set
-        local open_menu = require("cartola.utils.util-trouble").open_menu;
-        local trouble = require("trouble")
-        trouble.setup({
-            modes = {
-                lsp_base = {
-                    focus = true,
-                    params = {
-                        include_current = false,
-                    },
-                },
-                lsp_references = {
-                    params = {
-                        include_declaration = true,
-                    },
-                    events = {
-                        -- -- NOTE: uma boa ideia?
-                        -- { event = "TextChanged", main = true },
-                        -- { event = "LspAttach",   main = true },
-                    }
-                },
-                diagnostics_buffer = {
-                    mode = "diagnostics",
-                    filter = { buf = 0 },
-                    groups = {
-                        { "filename", format = "{file_icon} {basename:Title} {count}" },
-                    },
-                }
-            }
-        })
+    tag = "v3.7.1",
+    cmd = "Trouble",
+    keys = {
+        { "<leader>[t", function() require("trouble").next({ skip_groups = true, jump = true }) end,     desc = "[t]rouble: [t]oggle list" },
+        { "<leader>]t", function() require("trouble").previous({ skip_groups = true, jump = true }) end, desc = "[t]rouble: [t]oggle list" },
 
-        setmap("n", "<leader>tt", function() open_menu() end)
-        setmap("n", "<leader>tw", function() open_menu("diagnostics") end)
-        setmap("n", "<leader>td", function() open_menu("diagnostics_buffer") end)
-        setmap("n", "<leader>tq", function() open_menu("quickfix") end)
-        setmap("n", "<leader>tl", function() open_menu("loclist") end)
-        setmap("n", "<leader>[t", function() trouble.next({ skip_groups = true, jump = true }) end)
-        setmap("n", "<leader>[t", function() trouble.next({ skip_groups = true, jump = true }) end)
-        setmap("n", "<leader>[t", function() trouble.next({ skip_groups = true, jump = true }) end)
-        setmap("n", "<leader>]t", function() trouble.previous({ skip_groups = true, jump = true }) end)
-        setmap("n", "gr", function() open_menu("lsp_references") end)
-        setmap("n", "gd", function() open_menu("lsp_definitions") end)
-        setmap("n", "gi", function() open_menu("lsp_implementations") end)
+        { "<leader>tt", function() open_menu() end,                                                      desc = "[t]rouble: [t]oggle list" },
+        { "<leader>tw", function() open_menu("diagnostics") end,                                         desc = "[t]rouble: [w]orkspace diagnostics list" },
+        { "<leader>td", function() open_menu("diagnostics_buffer") end,                                  desc = "[t]rouble: buffer [d]iagnostics list" },
+        { "<leader>tq", function() open_menu("quickfix") end,                                            desc = "[t]rouble: [q]uickfix list" },
+        { "<leader>tl", function() open_menu("loclist") end,                                             desc = "[t]rouble: [l]ocation list" },
+
+        { "gr",         function() open_menu("lsp_references") end,                                      desc = "trouble: [g]o to [r]eferences" },
+        { "gd",         function() open_menu("lsp_definitions") end,                                     desc = "trouble: [g]o to [d]efinition" },
+        { "gi",         function() open_menu("lsp_implementations") end,                                 desc = "trouble: [g]o to [i]mplementation" },
+    },
+    opts = {
+        modes = {
+            lsp_base = {
+                focus = true,
+                follow = false,
+                auto_refresh = false,
+                restore = true,
+                params = {
+                    include_current = false,
+                },
+            },
+            lsp_references = {
+                params = {
+                    include_declaration = true,
+                },
+                events = {
+                    -- -- NOTE: uma boa ideia?
+                    -- { event = "TextChanged", main = true },
+                    -- { event = "LspAttach",   main = true },
+                }
+            },
+            diagnostics_buffer = {
+                mode = "diagnostics",
+                filter = { buf = 0 },
+                groups = {
+                    { "filename", format = "{file_icon} {basename:Title} {count}" },
+                },
+            }
+        }
+    },
+    config = function(_, opts)
+        require("trouble").setup(opts)
 
         vim.api.nvim_create_autocmd("BufRead", {
             callback = function(args)
