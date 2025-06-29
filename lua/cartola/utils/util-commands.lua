ab7f54810f65fbb5f0a4215ccaa88d63b05809c2
sawurora <sawurora@proton.me>
2025-06-29 06:01:17 -0300

    feat(commands): add Vimcfg command

---
+++ lua/cartola/utils/util-commands.lua
@@ -0,0 +1,25 @@
+vim.api.nvim_create_user_command('Vimcfg', function(opts)
+    local plugin = opts.args
+    local config = vim.fn.stdpath('config')
+    local path = config
+
+    if plugin ~= "" then
+        path = config .. '/lua/cartola/lazy/' .. plugin
+    end
+
+    vim.cmd('split' .. path)
+end, {
+    nargs = '?',
+    complete = function(_, _, _)
+        local lazy_path = vim.fn.stdpath('config') .. '/lua/cartola/lazy'
+        local dirs = vim.fn.globpath(lazy_path, '*', 0, 1)
+        local result = {}
+        for _, dir in ipairs(dirs) do
+            if vim.fn.isdirectory(dir) then
+                table.insert(result, vim.fn.fnamemodify(dir, ':t'))
+            end
+        end
+
+        return result
+    end
+})
