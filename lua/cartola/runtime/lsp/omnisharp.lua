7633fb986801a2e34b7f3eb66600915a9f437387
Carto1a <49589600+Carto1a@users.noreply.github.com>
2025-07-24 16:48:30 -0300

    feat(cs): lazy omnisharp-extended-lsp.nvim

--- lua/cartola/runtime/lsp/omnisharp.lua
+++ lua/cartola/runtime/lsp/omnisharp.lua
@@ -7,17 +7,4 @@ util.setup_default_settings(server_name, settings)
 
 local configuration = {}
 
-local ok, omnisharp_ext = pcall(require, "omnisharp_extended");
-
-if ok then
-    local handlers = {
-        ["textDocument/definition"] = omnisharp_ext.definition_handler,
-        ["textDocument/typeDefinition"] = omnisharp_ext.type_definition_handler,
-        ["textDocument/references"] = omnisharp_ext.references_handler,
-        ["textDocument/implementation"] = omnisharp_ext.implementation_handler,
-    }
-
-    configuration.handlers = handlers
-end
-
 return configuration
