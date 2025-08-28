e97d605f32c7e6b1ddbd37998c2e218fa994212e
Carto1a <49589600+Carto1a@users.noreply.github.com>
2025-08-28 14:30:52 -0300

    refactor(omnisharp): move omnisharp extended config to on init of lsp

--- lua/cartola/runtime/ftplugin/cs.lua
+++ lua/cartola/runtime/ftplugin/cs.lua
@@ -3,8 +3,3 @@ if vim.b.did_ftplugin == true then
 end
 
 vim.b.did_ftplugin = true
-
--- local ok, omnisharp_ext = pcall(require, "omnisharp_extended")
--- if ok then
---     vim.keymap.set('n', 'gd', omnisharp_ext.lsp_definition, { buffer = true })
--- end
--- lua/cartola/runtime/lsp/omnisharp.lua
+++ lua/cartola/runtime/lsp/omnisharp.lua
@@ -4,4 +4,11 @@ local server_name = "omnisharp"
 local settings = util.get_servers_settings(server_name)
 local configuration = settings.internal or {}
 
+configuration.on_init = function(_)
+    local ok, omnisharp_ext = pcall(require, "omnisharp_extended")
+    if ok then
+        vim.keymap.set('n', 'gd', omnisharp_ext.lsp_definition, { buffer = true })
+    end
+end
+
 return configuration
