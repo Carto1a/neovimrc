5e4d475c85e4a0ad36dba48fa94df9b206f18378
Carto1a <49589600+Carto1a@users.noreply.github.com>
2025-10-22 15:49:33 -0300

    feat(telescope): use live grep

--- lua/cartola/lazy/telescope.lua
+++ lua/cartola/lazy/telescope.lua
@@ -5,10 +5,10 @@ return {
         'nvim-lua/plenary.nvim'
     },
     keys = {
-        { "<leader>pf", function() require('telescope.builtin').find_files() end,                                       desc = "telescope: [p]roject [f]iles" },
-        { "<leader>pg", function() require('telescope.builtin').git_files() end,                                        desc = "telescope: [p]roject [g]it files" },
-        { "<leader>fb", function() require('telescope.builtin').buffers() end,                                          desc = "telescope: [f]ind [b]uffers" },
-        { "<leader>ps", function() require('telescope.builtin').grep_string({ search = vim.fn.input("Grep > ") }); end, desc = "telescope: [p]roject [s]earch (live grep)" } },
+        { "<leader>pf", function() require('telescope.builtin').find_files() end, desc = "telescope: [p]roject [f]iles" },
+        { "<leader>pg", function() require('telescope.builtin').git_files() end,  desc = "telescope: [p]roject [g]it files" },
+        { "<leader>fb", function() require('telescope.builtin').buffers() end,    desc = "telescope: [f]ind [b]uffers" },
+        { "<leader>ps", function() require('telescope.builtin').live_grep(); end, desc = "telescope: [p]roject [s]earch (live grep)" } },
     lazy = true,
     opts = {}
 }
